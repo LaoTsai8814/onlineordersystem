c3e6a31560c260b0f5c7ea94635f26591e465df8
laotsai <laotsai@WIN-OEMSS4DH5SI>
2026-07-16 13:32:04 +0800

    change enviroment setting

--- src/global/EnviromentDefine.ts
+++ src/global/EnviromentDefine.ts
@@ -1,4 +1,4 @@
 // src/api/config.ts
-//export const WebHostDomain = "https://onlineordersystem.duckdns.org/";
-export const WebHostDomain = "https://localhost:7214/";
+export const WebHostDomain = "https://onlineordersystem.duckdns.org/";
+//export const WebHostDomain = "https://localhost:7214/";
 export const FileHostDomain = "https://onlineordersystem.duckdns.org/";
